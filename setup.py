ce75c4027c6a62c231fd5820d797b180041c1c59
earlsuke <n21318@gmail.com>
2019-11-23 18:35:05 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 
 setup(
     name='lexpp',
-    version='0.0.3',
+    version='1.0.0',
     description='A lexical pre-processing module for Japanese text',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
@@ -11,4 +11,8 @@ setup(
     author_email='ryosuke.mitani@gmail.com',
     url='https://github.com/earlsuke',
     license="Apache-2.0",
+    packages=['lexpp', 'lexpp.res'],
+    include_package_data=True,
+    package_data={'lexpp':['res/*.dict']},
 )
+
